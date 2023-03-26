6e30c863e874634e5edffe8796571b3f1a04d19b
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-03-26 13:20:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 const http = require('http').Server(app);
 
-const socket = require('socket.io')(http, { cors: { origin: 'http://localhost:3000' } });
+const socket = require('socket.io')(http, { cors: { origin: 'https://socket-zilu.onrender.com' } });
 
 global.onlineUsers = new Map();
 
