7c192d65ef87f27613c5dd3eebd32c768b98b719
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-06-05 20:20:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 const http = require('http').Server(app);
 
 // const socket = require('socket.io')(http, { cors: { origin: 'https://socket-zilu.onrender.com' } });
-const socket = require('socket.io')(http, { cors: { origin: 'http://localhost:3000/chat' } });
+const socket = require('socket.io')(http, { cors: { origin: 'http://localhost:3000' } });
 // const socket = require('socket.io')(http, { cors: { origin: 'https://prime-chat.netlify.app/chat' } });
 
 global.onlineUsers = new Map();
