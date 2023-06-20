9fede7bd373e9892a9899a9f49d2caf376e4c29d
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-06-20 16:27:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 const http = require('http').Server(app);
 
-const socket = require('socket.io')(http, { cors: { origin: 'https://front-socket-chat.onrender.com/' } });
+const socket = require('socket.io')(http, { cors: { origin: 'http://localhost:3000' } });
 
 socket.on('connection', (user) =>
 {
